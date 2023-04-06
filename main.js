87b66dd0aa0f84f78c1c907dbc192787081388bc
rib9985 <rib9985@gmail.com>
2023-04-05 21:41:11 -0300

    Create library function

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 let myLibrary = [];
 
-function Book(title,author,pages,read){
+
+function Book(title,author,pages,read,id){
   this.title = title
   this.author= author
   this.pages= pages
   this.read= read
+  this.bookId = id
 }
 
 const form = document.getElementById('book');
+
+function getCurrentIndexNumber(){
+  let currentIndex = myLibrary.length
+return currentIndex
+}
+
 
 function addBookToLibrary() {
   const title = document.getElementById('title').value
   const author = document.getElementById('author').value
   const pages = document.getElementById('pages').value
   const read = document.getElementById('read').value === 'true';
-
-  const book = new Book(title, author, pages, read)
-
-  return myLibrary.push(book)
+  const id = getCurrentIndexNumber()
+  
+  const book = new Book(title, author, pages, read, id)
+  return myLibrary.push(book) 
+ 
 }
 
 function clearFormFields(){
@@ -28,11 +37,43 @@ function clearFormFields(){
 
 }
 
-function displayLibrary(){
+function displayBook(book){
+  const libraryContainer = document.getElementById('library');
+
+  const bookElement = document.createElement('div');
+  bookElement.classList.add('book-card')
+
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = book.title
+  titleElement.classList.add('book-card-title')
+  bookElement.appendChild(titleElement)
+
+  const authorElement = document.createElement('p');
+  authorElement.textContent = `Author: ${book.author}`
+  authorElement.classList.add('book-card-author')
+  bookElement.appendChild(authorElement)
+
+  const pagesElement = document.createElement('p');
+  pagesElement.textContent = `Pages: ${book.pages}`
+  pagesElement.classList.add('book-card-pages')
+  bookElement.appendChild(pagesElement)
+
+  const readElement = document.createElement('p');
+  readElement.textContent = `Read: ${book.read}`
+  readElement.classList.add('book-card-read')
+  bookElement.appendChild(pagesElement)
+
+  libraryContainer.appendChild(bookElement)
+
 
 }
 
+
+
+
+
 function removeBookFromLibrary(){
+
 
 }
 
@@ -41,9 +82,12 @@ function changeReadStatus(){
 }
 
 
+
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   addBookToLibrary()
   clearFormFields()
+  displayBook(myLibrary[`${getCurrentIndexNumber()-1}`])
 });
 
