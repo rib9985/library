f3ad98908d8f650c89547840582451be8bdeab80
rib9985 <rib9985@gmail.com>
2023-04-06 11:26:20 -0300

    Add eslint and linting to main.js

--- main.js
+++ main.js
@@ -1,159 +1,135 @@
-let myLibrary = [];
+const myLibrary = [];
 
-
-function Book(title,author,pages,read,id){
-  this.title = title
-  this.author= author
-  this.pages= pages
-  this.read= read
-  this.id = id
+function Book(title, author, pages, read, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
 }
 
 const form = document.getElementById('book');
 
-function getCurrentIndexNumber(){
-  let currentIndex = myLibrary.length
-return currentIndex
+function getCurrentIndexNumber() {
+  const currentIndex = myLibrary.length;
+  return currentIndex;
 }
-
 
 function addBookToLibrary() {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
   const read = document.getElementById('read').value === 'true';
-  const id = getCurrentIndexNumber()
-  
-  const book = new Book(title, author, pages, read, id)
-  return myLibrary.push(book) 
- 
+  const id = getCurrentIndexNumber();
+
+  const book = new Book(title, author, pages, read, id);
+  return myLibrary.push(book);
 }
-
-function clearFormFields(){
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('pages').value = '';
-  document.getElementById('read').value = 'true';
-
-}
-
-function createBookElement(book){
-  const libraryContainer = document.getElementById('library');
-
-  const bookElement = document.createElement('div');
-  bookElement.classList.add(`book-card-${book.id}`)
-  bookElement.setAttribute('id',`${book.id}`)
-
-
-  const titleElement = document.createElement('h3');
-  titleElement.textContent = book.title
-  titleElement.classList.add('book-card-title')
-  bookElement.appendChild(titleElement)
-
-  const authorElement = document.createElement('p');
-  authorElement.textContent = `Author: ${book.author}`
-  authorElement.classList.add('book-card-author')
-  bookElement.appendChild(authorElement)
-
-  const pagesElement = document.createElement('p');
-  pagesElement.textContent = `Pages: ${book.pages}`
-  pagesElement.classList.add('book-card-pages')
-  bookElement.appendChild(pagesElement)
-
-
-  const readButton = document.createElement('button')
-  const status = checkBookReadStatus(book.read)
-  if (status === true){
-    readButton.textContent = "Book Read"
-    readButton.classList.add('book-read-true')
-  }
-  else{
-    readButton.textContent = "Book Not Read"
-    readButton.classList.add('book-read-false')
-  }
-
-  readButton.onclick=function(){
-    const status = checkBookReadStatus(book.read)
-    if (status !== true){
-      readButton.textContent = "Book Read"
-      readButton.classList.add('book-read-true')
-      return book.read=true
-    }
-    else{
-      readButton.textContent = "Book Not Read"
-      readButton.classList.add('book-read-false')
-      return book.read=false
-    }
-    
-  }
-
-  bookElement.append(readButton)
-
-
- 
-
-  const removeBook = document.createElement('button');
-  removeBook.textContent = 'Remove Book'
-  removeBook.id.replace('remove')
-  removeBook.onclick=function(){removeBookFromLibrary(`${book.id}`)}
-  bookElement.appendChild(removeBook)
-
- 
-
-  libraryContainer.appendChild(bookElement)
-
-
-}
-
-function libraryDisplay(){
-  const libraryContainer = document.getElementById('library');
-  libraryContainer.innerHTML=""
-  
-myLibrary.forEach(function(book){
-  createBookElement(book)
-})
-
-}
-
-
-
-function removeBookFromLibrary(book){
-  removeBookFromDisplay(book)
-  myLibrary.splice(book,1)
-  updatePositions()
-  libraryDisplay()
-
-  
-
-}
-
-function removeBookFromDisplay(book){
-  const bookDiv = document.getElementById(`${book}`)
-  bookDiv.remove()
- 
-}
-
 
 function updatePositions() {
-  myLibrary.forEach(function(book, index) {
+  myLibrary.forEach((book, index) => {
     book.id = index;
   });
 }
 
-
-function checkBookReadStatus(book){
-  if (book === true) {return true}
-  else {return false}
+function removeBookFromDisplay(book) {
+  const bookDiv = document.getElementById(`${book}`);
+  bookDiv.remove();
 }
 
+function removeBookFromLibrary(book) {
+  removeBookFromDisplay(book);
+  myLibrary.splice(book, 1);
+  updatePositions();
+  libraryDisplay();
+}
 
+function checkBookReadStatus(book) {
+  if (book === true) {
+    return true;
+  }
+  return false;
+}
 
-form.addEventListener('submit', function(event) {
+function clearFormFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+  document.getElementById('read').value = 'true';
+}
+
+function createBookElement(book) {
+  const libraryContainer = document.getElementById('library');
+
+  const bookElement = document.createElement('div');
+  bookElement.classList.add(`book-card-${book.id}`);
+  bookElement.setAttribute('id', `${book.id}`);
+
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = book.title;
+  titleElement.classList.add('book-card-title');
+  bookElement.appendChild(titleElement);
+
+  const authorElement = document.createElement('p');
+  authorElement.textContent = `Author: ${book.author}`;
+  authorElement.classList.add('book-card-author');
+  bookElement.appendChild(authorElement);
+
+  const pagesElement = document.createElement('p');
+  pagesElement.textContent = `Pages: ${book.pages}`;
+  pagesElement.classList.add('book-card-pages');
+  bookElement.appendChild(pagesElement);
+
+  const readButton = document.createElement('button');
+  const status = checkBookReadStatus(book.read);
+  if (status === true) {
+    readButton.textContent = 'Book Read';
+    readButton.classList.add('book-read-true');
+  } else {
+    readButton.textContent = 'Book Not Read';
+    readButton.classList.add('book-read-false');
+  }
+
+  readButton.onclick = function () {
+    const status = checkBookReadStatus(book.read);
+    if (status !== true) {
+      readButton.textContent = 'Book Read';
+      readButton.classList.add('book-read-true');
+      book.read = true;
+      return book.read;
+    }
+    readButton.textContent = 'Book Not Read';
+    readButton.classList.add('book-read-false');
+    book.read = false;
+    return book.read;
+  };
+
+  bookElement.append(readButton);
+
+  const removeBook = document.createElement('button');
+  removeBook.textContent = 'Remove Book';
+  removeBook.id.replace('remove');
+  removeBook.onclick = function () {
+    removeBookFromLibrary(`${book.id}`);
+  };
+  bookElement.appendChild(removeBook);
+
+  libraryContainer.appendChild(bookElement);
+}
+
+function libraryDisplay() {
+  const libraryContainer = document.getElementById('library');
+  libraryContainer.innerHTML = '';
+
+  myLibrary.forEach((book) => {
+    createBookElement(book);
+  });
+}
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  addBookToLibrary()
-  updatePositions()
-  clearFormFields()
-  libraryDisplay()
-
+  addBookToLibrary();
+  updatePositions();
+  clearFormFields();
+  libraryDisplay();
 });
-
